4e3aadd0d70578bb1a976a9cd48c5838f6a84906
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-11-16 17:10:57 -0500

    ani wolf

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -12,196 +12,196 @@ import {gsap} from "gsap";
 const drawInTL = gsap.timeline();
 
 
-// drawInTL.from("#A-1", {
-//         duration: 3,
-//         x: -50,
-//         y: -140,
-//         transformOrigin: "center"
-//     }, "play");
+drawInTL.from("#A-1", {
+        duration: 3,
+        x: -50,
+        y: -140,
+        transformOrigin: "center"
+    }, "play");
 
-// drawInTL.from("#H-1", {
-//     duration: 3.5,
-//     x: 50,
-//     y: -180,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#H-1", {
+    duration: 3.5,
+    x: 50,
+    y: -180,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#A-2", {
-//     duration: 3.25,
-//     x: 20,
-//     y: -190,
-//     scale: -2,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#A-2", {
+    duration: 3.25,
+    x: 20,
+    y: -190,
+    scale: -2,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#U-1", {
-//     duration: 4,
-//     x: -70,
-//     y: -200,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#U-1", {
+    duration: 4,
+    x: -70,
+    y: -200,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#N-1", {
-//     duration: 3.75,
-//     x: 10,
-//     y: -180,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#N-1", {
+    duration: 3.75,
+    x: 10,
+    y: -180,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#T-1", {
-//     duration: 4.25,
-//     x: -30,
-//     y: -140,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#T-1", {
+    duration: 4.25,
+    x: -30,
+    y: -140,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#E-1", {
-//     duration: 2.5,
-//     x: 10,
-//     y: -170,
-//     scale: 1.5,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#E-1", {
+    duration: 2.5,
+    x: 10,
+    y: -170,
+    scale: 1.5,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#D-1", {
-//     duration: 3,
-//     x: 40,
-//     y: -210,
-//     transformOrigin: "center"
-// }, "play");
-
-
-
-// drawInTL.from("#H-2", {
-//     duration: 3,
-//     x: 40,
-//     y: 350,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#O-1", {
-//     duration: 3.5,
-//     x: 10,
-//     y: 340,
-//     scale: 1.5,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#U-2", {
-//     duration: 4.25,
-//     x: -30,
-//     y: 340,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#S-1", {
-//     duration: 3,
-//     x: 40,
-//     y: 310,
-//     scale: .5,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#E-2", {
-//     duration: 3.75,
-//     x: 10,
-//     y: 380,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#D-1", {
+    duration: 3,
+    x: 40,
+    y: -210,
+    transformOrigin: "center"
+}, "play");
 
 
 
-// drawInTL.from("#V-1", {
-//     duration: 3.25,
-//     x: -30,
-//     y: 330,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#H-2", {
+    duration: 3,
+    x: 40,
+    y: 350,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#I-1", {
-//     duration: 3.5,
-//     x: 50,
-//     y: 320,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#O-1", {
+    duration: 3.5,
+    x: 10,
+    y: 340,
+    scale: 1.5,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#R-1", {
-//     duration: 3,
-//     x: -35,
-//     y: 320,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#U-2", {
+    duration: 4.25,
+    x: -30,
+    y: 340,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#G-1", {
-//     duration: 2.8,
-//     x: 25,
-//     y: 320,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#S-1", {
+    duration: 3,
+    x: 40,
+    y: 310,
+    scale: .5,
+    transformOrigin: "center"
+}, "play");
 
-// drawInTL.from("#I-2", {
-//     duration: 3.65,
-//     x: 10,
-//     y: 320,
-//     scale: 3,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#N-2", {
-//     duration: 3.25,
-//     x: -20,
-//     y: 300,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#I-3", {
-//     duration: 3.5,
-//     x: 20,
-//     y: 320,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#A-3", {
-//     duration: 3.15,
-//     x: -10,
-//     y: 330,
-//     scale: 2,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#W-1", {
-//     duration: 3,
-//     x: 45,
-//     y: 320,
-//     scale: .2,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#O-2", {
-//     duration: 3.5,
-//     x: 65,
-//     y: 320,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#L-1", {
-//     duration: 3.75,
-//     x: 15,
-//     y: 320,
-//     transformOrigin: "center"
-// }, "play");
-
-// drawInTL.from("#F-1", {
-//     duration: 3.25,
-//     x: -65,
-//     y: 320,
-//     transformOrigin: "center"
-// }, "play");
+drawInTL.from("#E-2", {
+    duration: 3.75,
+    x: 10,
+    y: 380,
+    transformOrigin: "center"
+}, "play");
 
 
 
+drawInTL.from("#V-1", {
+    duration: 3.25,
+    x: -30,
+    y: 330,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#I-1", {
+    duration: 3.5,
+    x: 50,
+    y: 320,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#R-1", {
+    duration: 3,
+    x: -35,
+    y: 320,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#G-1", {
+    duration: 2.8,
+    x: 25,
+    y: 320,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#I-2", {
+    duration: 3.65,
+    x: 10,
+    y: 320,
+    scale: 3,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#N-2", {
+    duration: 3.25,
+    x: -20,
+    y: 300,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#I-3", {
+    duration: 3.5,
+    x: 20,
+    y: 320,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#A-3", {
+    duration: 3.15,
+    x: -10,
+    y: 330,
+    scale: 2,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#W-1", {
+    duration: 3,
+    x: 45,
+    y: 320,
+    scale: .2,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#O-2", {
+    duration: 3.5,
+    x: 65,
+    y: 320,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#L-1", {
+    duration: 3.75,
+    x: 15,
+    y: 320,
+    transformOrigin: "center"
+}, "play");
+
+drawInTL.from("#F-1", {
+    duration: 3.25,
+    x: -65,
+    y: 320,
+    transformOrigin: "center"
+}, "play");
 
 
+
+
+drawInTL.delay(2).play
 
 
 
@@ -318,91 +318,91 @@ drawInTL.to("#E-2", {
 
 
 
-// drawInTL.from("#V-1", {
-// duration: 3.25,
-// x: -30,
-// y: 330,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#V-1", {
+duration: 3.25,
+x: -30,
+y: 80,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#I-1", {
-// duration: 3.5,
-// x: 50,
-// y: 320,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#I-1", {
+duration: 3.5,
+x: 65,
+y: -10,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#R-1", {
-// duration: 3,
-// x: -35,
-// y: 320,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#R-1", {
+duration: 3,
+x: -35,
+y: 60,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#G-1", {
-// duration: 2.8,
-// x: 25,
-// y: 320,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#G-1", {
+duration: 2.8,
+x: 25,
+y: 45,
+scale: 2,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#I-2", {
-// duration: 3.65,
-// x: 10,
-// y: 320,
-// scale: 3,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#I-2", {
+duration: 3.65,
+x: 10,
+y: 80,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#N-2", {
-// duration: 3.25,
-// x: -20,
-// y: 300,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#N-2", {
+duration: 3.25,
+x: -20,
+y: -20,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#I-3", {
-// duration: 3.5,
-// x: 20,
-// y: 320,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#I-3", {
+duration: 3.5,
+x: 20,
+y: 60,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#A-3", {
-// duration: 3.15,
-// x: -10,
-// y: 330,
-// scale: 2,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#A-3", {
+duration: 3.15,
+x: -10,
+y: 130,
+scale: 2,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#W-1", {
-// duration: 3,
-// x: 45,
-// y: 320,
-// scale: .2,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#W-1", {
+duration: 3,
+x: 45,
+y: 20,
+scale: .5,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#O-2", {
-// duration: 3.5,
-// x: 65,
-// y: 320,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#O-2", {
+duration: 3.5,
+x: 65,
+y: -55,
+transformOrigin: "center"
+}, "play2");
 
-// drawInTL.from("#L-1", {
-// duration: 3.75,
-// x: 15,
-// y: 320,
-// transformOrigin: "center"
-// }, "play");
+drawInTL.to("#L-1", {
+duration: 3.75,
+x: 15,
+y: 30,
+transformOrigin: "center"
+}, "play2");
 
 drawInTL.to("#F-1", {
 duration: 3.25,
-x: -220,
-y: -170,
-scale:1.5,
+x: -250,
+y: -140,
+scale:1.75,
 transformOrigin: "center"
 }, "play2");
 
